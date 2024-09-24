c901eed0751ed0948e941e7c096c3aa01d46a524
FranciellyDiasM <frandiasma@gmail.com>
2024-09-24 14:13:34 -0300

    Implementacao do ContaLetra

--- src/desafio/ContaLetra.java
+++ src/desafio/ContaLetra.java
@@ -5,6 +5,13 @@ public class ContaLetra {
     public int contaA(String entrada) {
         int contador = 0;
 
+        for (int i = 0; i < entrada.length(); i++) {
+            char letra = entrada.charAt(i);
+            if (letra == 'a' || letra == 'A') {
+                contador++;
+            }
+        }
+
         return contador;
     }
 }
