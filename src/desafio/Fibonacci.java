f0382d9f3c39eb1f11fb9f1f4b17e21a0591859d
FranciellyDiasM <frandiasma@gmail.com>
2024-09-24 13:49:21 -0300

    implementacao do fibonacci

--- src/desafio/Fibonacci.java
+++ src/desafio/Fibonacci.java
@@ -3,6 +3,19 @@ package desafio;
 public class Fibonacci {
 
     public boolean pertence(int numero) {
-        return false;
+        int num1 = 0;
+        int num2 = 1;
+
+        if (numero == num1 || numero == num2) {
+            return true;
+        }
+
+        while (num2 < numero) {
+            int proximo = num1 + num2;
+            num1 = num2;
+            num2 = proximo;
+        }
+
+        return num2 == numero;
     }
 }
